34aafe5c00fc37b0b91e5564c210ad6994bd872f
Rina Velasquez <86273468+rinvel7@users.noreply.github.com>
2023-11-03 03:39:19 -0400

    Add files via upload
    Finish project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Interface em React
+
+tela-de-cadastro-dio
+Proyecto Base: Desenvolvendo a Tela de Cadastro da Plataforma Dio com React
+#Tela-de-cadastro
---
+++ src/pages/cadastro/styles.js
@@ -0,0 +1,90 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+    width: 100%;
+    max-width: 80%;
+    margin: 0 auto;
+    margin-top: 120px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: top;
+`
+
+export const Wrapper = styled.div`
+    max-width: 300px;
+`
+
+export const Column = styled.div`
+    flex: 1;
+    
+`
+
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 20px;
+`
+
+export const Title = styled.h2`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 32px;
+    max-width: 90%;
+    margin-bottom: 20px;
+    line-height: 44px;
+    color: #FFFFFF;
+    text-align: left;
+`
+
+export const TitleLogin = styled.p`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 29px;
+    line-height: 44px;
+    margin-bottom: 8px;
+`
+
+export const SubtitleLogin = styled.p`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 17px;
+    line-height: 25px;
+    margin-bottom: 35px;
+`
+
+export const AdicionalText = styled.p`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    max-width: 100%;
+    line-height: 25px;
+    color: #FFFFFF;
+    margin-top: 20px;
+`
+
+export const ContaText  = styled.p`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 19px;
+    color: #FFFFFF;
+    display: flex;
+` 
+
+export const CriarText =  styled.a`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 19px;
+    color: #23DD7A;
+    margin-left: 10px;
+`
